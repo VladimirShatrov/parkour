de099f847b1aafde5c17d58f1b712198a5994336
VladimirShatrov <vova.shatrov.2005@gmail.com>
2025-03-31 19:17:04 +0500

    init

---
+++ src/test/java/makrket/analyses/parkour/ParkourApplicationTests.java
@@ -0,0 +1,13 @@
+package makrket.analyses.parkour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
